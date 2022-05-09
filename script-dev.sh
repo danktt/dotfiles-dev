eb753f84398de40951c811324aa24bce6ed32956
Danilo Miranda <60451984+danktt@users.noreply.github.com>
2022-05-09 09:25:49 -0300

    Update script-dev.sh
    KeepUp

--- script-dev.sh
+++ script-dev.sh
@@ -44,10 +44,11 @@ wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format
 sudo apt install ~/discord.deb -y
 
 echo "############################## Instalando o Spotify ##############################"
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update
-sudo apt-get install spotify-client -y
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+sudo apt-get update && sudo apt-get install spotify-client -y
+
 
 echo "Instalando o NVM"
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
